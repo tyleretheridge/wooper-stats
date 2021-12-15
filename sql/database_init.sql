94e50c45679d023f500ee31683a08e2e852451c8
tyleretheridge <tylerjetheridge98@gmail.com>
2021-12-15 17:11:33 -0500

    add sql file for table init in docker

---
+++ sql/database_init.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS twitch;
+CREATE TABLE IF NOT EXISTS twitch.streams (
+    user_id VARCHAR(50),
+    user_login VARCHAR(50),
+    game_name VARCHAR(50),
+    viewer_count INT,
+    language VARCHAR,
+    is_mature BOOLEAN,
+    date DATETIME
+    PRIMARY KEY (user_id)
+);
