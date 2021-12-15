3e69c6c5c443d30487e6fe0083f9a36cb5b0ffa8
tyleretheridge <tylerjetheridge98@gmail.com>
2021-12-14 23:17:29 -0500

    consolidate env loading

--- src/utils/auth_config.py
+++ src/utils/auth_config.py
@@ -1,5 +1,5 @@
 import os
-
+from dotenv import dotenv_values, load_dotenv
 
 def load_warehouse_credentials():
     credentials = {
@@ -11,3 +11,24 @@ def load_warehouse_credentials():
     }
 
     return credentials
+
+
+def get_auth():
+	
+	# Load secret form .env
+	keys = dotenv_values(".env")
+
+	# Set params for API token retrieval
+	URL = "https://id.twitch.tv/oauth2/token"
+	PARAMS = {
+	"client_id": keys["CLIENT_ID"],
+	"client_secret": keys["CLIENT_SECRET"],
+	"grant_type": "client_credentials"
+	}
+
+	# POST request for token
+	r = requests.post(url = URL, params = PARAMS)
+	# Fetch token from return data
+	access_token = r.json()["access_token"]
+
+	return keys["CLIENT_ID"], access_token
